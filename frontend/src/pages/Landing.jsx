8f3e35f9386fbf840654f18254ea62a5aaa581c8
NSuryansh <nagar.suryansh@gmail.com>
2025-03-16 16:42:24 +0530

    Get started button desgin sorted

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Landing() {
     return (
-        <div className="flex flex-col items-center justify-center mt-[70px]">
+        <div className="flex flex-col items-center justify-center mt-[60px]">
             <div
                 className="text-[60px] font-extrabold text-[var(--landing-text-orange)]" >
                 CalmNest
@@ -12,14 +12,17 @@ export default function Landing() {
                 Find Peace - Get Support - Thrive
             </div>
             <div
-                className="text-[40px] mt-2 flex flex-col items-center text-[var(--landing-text-black)]">
+                className="text-[35px] mt-2 flex flex-col items-center text-[var(--landing-text-black)]">
                 <p>At CalmNest we provide a safe space for your mental welness journey.</p>
                 <p>Wheather you are seeking mindfulness techniques, emotional support, or expert</p>
                 <p>guidance, we are here to help</p>
             </div>
-            <button className="border-black">Get Started</button>
+            <button className="bg-[var(--landing-bg-orange)] text-white font-semibold px-6 py-2 mt-4 rounded-full shadow-md 
+                   hover:bg-transparent transition-colors duration-300 ease-in-out">
+                Get Started
+            </button>
 
-            <img src="/assests/plates.png" alt="" className="h-[180px]"/>
+            <img src="/assests/plates.png" alt="" className="h-[180px]" />
         </div>
     )
 }
