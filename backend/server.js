6b432c24a1f3536a5dc7250785f13961147f0431
NSuryansh <nagar.suryansh@gmail.com>
2025-03-17 22:39:10 +0530

    login functionalty added

--- backend/server.js
+++ backend/server.js
@@ -2,73 +2,79 @@ import express from "express"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 import { PrismaClient } from "@prisma/client"
+import cors from "cors"
+
+
 
 const prisma = new PrismaClient()
 const app = express()
 app.use(express.json());
 const port = 3000
-const SECRET_KEY="hT9XpzU2Z7yNdD9J7jR1bC5qW1J2sDklFPLV2hOx6pY="
+const SECRET_KEY = "hT9XpzU2Z7yNdD9J7jR1bC5qW1J2sDklFPLV2hOx6pY="
 
-app.post('/signup', async(req,res)=>{
+app.use(cors())
+
+app.post('/signup', async (req, res) => {
     const username = req.body['username']
     const email = req.body['email']
     const mobile = req.body['mobile']
     const password = req.body['password']
     const altNo = req.body['altNo']
-    
+
     const hashedPassword = await bcrypt.hash(password, 10)
 
-    try{
+    try {
         const user = await prisma.user.create({
-            data:{
-                username:username,
-                mobile:mobile,
-                email:email,
-                password:hashedPassword,
-                alt_mobile:altNo
+            data: {
+                username: username,
+                mobile: mobile,
+                email: email,
+                password: hashedPassword,
+                alt_mobile: altNo
             }
         })
-        res.status(201).json({message:"User added"})
-    }catch(e){
+        res.status(201).json({ message: "User added" })
+    } catch (e) {
         console.log(e)
     }
 })
 
-app.post('/login', async(req, res)=>{
+app.post('/login', async (req, res) => {
+    console.log(req.body)
     const username = req.body["username"]
     const password = req.body["password"]
 
-    const user = await prisma.user.findUnique({where:{username:username}})
-    if(!user){
-        return res.status(401).json({message:"User doesn't exists"})
+    const user = await prisma.user.findUnique({ where: { username: username } })
+    if (!user) {
+        return res.status(401).json({ message: "User doesn't exists" })
     }
     const match = await bcrypt.compare(password, user.password)
-    if(!match){
-        return res.status(401).json({message:"Incorrect password"})
+    if (!match) {
+        return res.status(401).json({ message: "Incorrect password" })
     }
-    const token = jwt.sign({ userId: user.id, username:user.username, email:user.email }, SECRET_KEY, {
+    const token = jwt.sign({ userId: user.id, username: user.username, email: user.email }, SECRET_KEY, {
         expiresIn: "1h",
     });
 
-    res.json({message:"Login successful", token})
+    res.json({ message: "Login successful", token })
 })
 
-app.get('/profile', async(req,res)=>{
+app.get('/profile', async (req, res) => {
     const token = req.headers.authorization?.split(" ")[1]
-    if(!token){
+    if (!token) {
         console.log(token)
-        return res.status(401).json({message:"Unauthorized", token})
+        return res.status(401).json({ message: "Unauthorized", token })
     }
-    try{
+    try {
         const decoded = jwt.verify(token, SECRET_KEY)
-        const user = await prisma.user.findUnique({where:{username:decoded.username}})
+        const user = await prisma.user.findUnique({ where: { username: decoded.username } })
         console.log(user)
         res.json(JSON.parse(JSON.stringify(user)))
-    }catch(e){
+    } catch (e) {
         console.log(e)
     }
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,48 @@
+import { set } from "date-fns";
 import React from "react";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  return (
-   
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handlelogin = async() => {
+    const response = await(fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ 
+        "username": username, "password": password 
+      })
+    }));
+    console.log(response);
+    const res = await response.json()
+    console.log(res)
+    
+      if(res["message"] == "Login successful"){
+        console.log(res["message"]);
+        const response2 = await(fetch("http://localhost:3000/profile", {
+          method: "GET",
+          headers: { "Authorization": "Bearer " + res["token"] }
+        }));
+        const res2 = await response2.json();
+        console.log(res2);
+        if(res2["message"] == "Unauthorized"){
+          console.log(res2["message"]);
+        }
+        else{
+          console.log(res2["message"]);
+          navigate("/dashboard");
+        }
+      }
+    else{
+      setError(res["message"]);
+    }
+  }
+  return ( 
     <div className="flex items-center justify-center h-screen ">
       <div className="bg-[var(--login-bg-peach)] w-80 p-10 rounded-md shadow-md">
         <h1 className="text-2xl font-bold text-[var(--login-text-color)] mb-4 text-center">
@@ -12,15 +51,19 @@ export default function Login() {
 
         <input type="text" placeholder="username"
           className="w-full mb-8 p-3  border border-[var(--login-input-border)] bg-[var(--login-input-bg)]
-            rounded-md"/>
+            rounded-md"
+          // value={username}
+          onChange={(e)=>setUsername(e.target.value)}/>
         
         <input type="password" placeholder="password"
           className="w-full mb-8 p-3 border border-[var(--login-input-border)] bg-[var(--login-input-bg)]
-            rounded-md"/>
+            rounded-md"
+          // value={password}
+          onChange={(e)=>setPassword(e.target.value)}/>
 
         <button
           className="w-full py-2 bg-[var(--login-button-orange)] text-[var(--login-button-text)]
-            font-semibold rounded-md hover:opacity-90 transition-opacity">
+            font-semibold rounded-md hover:opacity-90 transition-opacity cursor-pointer" onClick={handlelogin}>
           Login
         </button>
 
