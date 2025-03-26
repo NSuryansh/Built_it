963d25cf4d2fbff06f65ce8151cffcf9e031e7fb
Kartikey Raghav <kartikeyraghav2006@gmail.com>
2025-03-26 16:12:20 +0530

    Updated login screens

--- frontend/src/pages/admin/amdin_login.jsx
+++ frontend/src/pages/admin/amdin_login.jsx
@@ -32,7 +32,7 @@ const AdminLogin = () => {
 
   return (
     <div className="min-h-screen bg-[var(--custom-primary-green-50)] flex items-center justify-center p-4">
-      <div className="max-w-md w-full space-y-8 bg-[var(--custom-white)] p-8 rounded-xl shadow-lg">
+      <div className="max-w-md w-full space-y-2 bg-[var(--custom-white)] p-8 rounded-xl shadow-lg">
         <div className="text-center">
           <div className="flex justify-center">
             <div className="p-3 bg-[var(--custom-primary-green-100)] rounded-full">
@@ -42,11 +42,7 @@ const AdminLogin = () => {
           <h2 className="mt-4 text-3xl font-bold text-[var(--custom-primary-green-900)]">
             Admin Login
           </h2>
-          {error && (
-            <p className="mt-2 text-red-600 text-sm">
-              {error}
-            </p>
-          )}
+          {error && <p className="mt-2 text-red-600 text-sm">{error}</p>}
         </div>
 
         <form className="mt-8 space-y-6" onSubmit={handlelogin}>
@@ -95,6 +91,39 @@ const AdminLogin = () => {
             Login
           </button>
         </form>
+        <p className="mt-6 text-sm text-center text-[var(--login-text-color)]">
+          If not registered{" "}
+          <a
+            href="/signup"
+            className="underline font-bold text-[var(--custom-primary-green)]"
+          >
+            click here
+          </a>
+        </p>
+
+        <div className="flex justify-center mt-2 items-center text-[var(--login-light-text)]">
+          <hr className="flex-1/3" />
+          <div className="flex-1/3 text-center">OR</div>
+          <hr className="flex-1/3" />
+        </div>
+        <div className="flex w-full mt-2">
+          <p className="w-full text-center">
+            Login as a&nbsp;
+            <a
+              href="/doctor/login"
+              className="underline font-bold text-[var(--custom-primary-green)]"
+            >
+              Doctor
+            </a>
+            &nbsp;or&nbsp;
+            <a
+              href="/admin/login"
+              className="underline font-bold text-[var(--custom-primary-green)]"
+            >
+              Admin
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
