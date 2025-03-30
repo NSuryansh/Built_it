64d4f5bfabae32b2a78cbc616a9b634edb10ff71
YATHARTH-77 <yatharthm.2006@gmail.com>
2025-03-31 04:12:59 +0530

    admin_doctor_profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ import EventsList from "./pages/admin/admin_event_list";
 import AddEvent from "./pages/admin/admin_add_event";
 import AddDoctor from "./pages/admin/admin_add_doctor";
 import Appointments from "./pages/Appointments";
+import Profile from './pages/admin/admin_doctor_profile'
 import Stress from "./pages/stress";
 import Events from "./pages/Events";
 import ModifyProfile from "./pages/Modify_profile";
@@ -25,6 +26,7 @@ import ResetPassword from "./pages/ResetPassword";
 import DoctorResetPassword from "./pages/doctor/ResetPassword_doctor";
 import AdminResetPassword from "./pages/admin/admin_reset_password";
 import { ToastContainer } from "react-toastify";
+import { Profiler } from "react";
 
 export default function App() {
   const location = useLocation();
@@ -68,6 +70,7 @@ export default function App() {
         <Route path="/admin/reset_password" element={<AdminResetPassword />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/doctor_list" element={<DoctorsList />} />
+        <Route path="/admin/doctor_profile" element={<Profile />} />
         <Route path="/admin/event_list" element={<EventsList />} />
         <Route path="/admin/add_event" element={<AddEvent />} />
         <Route path="/admin/add_doctor" element={<AddDoctor />} />
