99ad05074b5d0c1afde2067a02715935de207d44
NSuryansh <nagar.suryansh@gmail.com>
2025-04-02 05:18:44 +0530

    ok

--- frontend/public/registerSW.js
+++ frontend/public/registerSW.js
@@ -3,3 +3,42 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/sw.js", { scope: "/" });
   });
 }
+
+const send_message_to_sw = (msg) =>
+  new Promise(
+    (resolve, reject) => {
+      // Create a Message Channel
+      const msg_chan = new MessageChannel();
+
+      // Handler for recieving message reply from service worker
+      msg_chan.port1.onmessage = (event) => {
+          if(event.data.error){
+              reject(event.data.error);
+          }else{
+              resolve(event.data);
+          }
+      };
+
+      // Send message to service worker along with port for reply
+      navigator.serviceWorker.controller.postMessage(
+        msg
+        , [msg_chan.port2]
+      );
+  }
+);
+
+document.body.addEventListener(
+  "click"
+  ,()=>
+    send_message_to_sw(
+      {
+        action:"delete"
+        ,cache:/^v1$/
+        ,url:/.*bundle.js$/
+      }
+    )
+    .then(
+      (msg)=>
+        console.log("deleted:",msg)
+    )
+);
