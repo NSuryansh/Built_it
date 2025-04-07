df6691e4501921262f7ed98da4a14999bb2f4550
Tanvi Agarwal <agarwaltanvi1507@gmail.com>
2025-04-08 04:14:09 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -408,11 +408,11 @@ app.get("/chatContacts", async (req, res) => {
 });
 
 app.get('/countUnseen', async (req, res) => {
-  const {userId} = req.query
+  const {userId, senderType} = req.query
   // const doctorId = Number(req.body['doctorId'])
-  const sender = req.body['senderType']
+  // const sender = req.body['senderType']
 
-  if (sender == "user") {
+  if (senderType == "user") {
     const unreadCount = await prisma.message.groupBy({
       by: ['senderId'],
       where: {
@@ -424,7 +424,7 @@ app.get('/countUnseen', async (req, res) => {
       }
     })
     res.json(unreadCount)
-  } else if (sender == "doc") {
+  } else if (senderType == "doc") {
     const unreadCount = await prisma.message.groupBy({
       by: ['senderId'],
       where: {
