cd07939c22b95d64a8108c6f2d77d023fd297309
Manan Jiwnani <mananjiwnani@gmail.com>
2025-04-24 00:21:18 +0530

    firebase notif fix

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -2,11 +2,11 @@ importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAmaUTaZr1rp_2kYzu4jEOo859YEA4OVls",
+  authDomain: "vitality-71a0e.firebaseapp.com",
+  projectId: "vitality-71a0e",
+  messagingSenderId: "908490427241",
+  appId: "1:908490427241:web:e4b7f255fddae0f4cb4bf8",
 });
 
 const messaging = firebase.messaging();
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    apiKey: "AIzaSyAmaUTaZr1rp_2kYzu4jEOo859YEA4OVls",
+    authDomain: "vitality-71a0e.firebaseapp.com",
+    projectId: "vitality-71a0e",
+    storageBucket: "vitality-71a0e.firebasestorage.app",
+    messagingSenderId: "908490427241",
+    appId: "1:908490427241:web:e4b7f255fddae0f4cb4bf8",
+    measurementId: "G-HLPJEN5M00"
 };
 
 const app = initializeApp(firebaseConfig);
