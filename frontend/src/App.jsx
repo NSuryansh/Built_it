6408631dfbcbbe3423f97edbc6851f22fdcf3280
Tanvi Agarwal <agarwaltanvi1507@gmail.com>
2025-04-24 01:21:30 +0530

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,17 +41,17 @@ import { initializeApp } from "firebase/app";
 export default function App() {
   const SERVER_KEY = import.meta.env.VITE_PUBLIC_VAPID_KEY;
 
-  firebase.initializeApp({
-    apiKey: "AIzaSyAmaUTaZr1rp_2kYzu4jEOo859YEA4OVls",
-    authDomain: "vitality-71a0e.firebaseapp.com",
-    projectId: "vitality-71a0e",
-    messagingSenderId: "908490427241",
-    appId: "1:908490427241:web:e4b7f255fddae0f4cb4bf8",
-  });
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyAmaUTaZr1rp_2kYzu4jEOo859YEA4OVls",
+//     authDomain: "vitality-71a0e.firebaseapp.com",
+//     projectId: "vitality-71a0e",
+//     messagingSenderId: "908490427241",
+//     appId: "1:908490427241:web:e4b7f255fddae0f4cb4bf8",
+//   };
 
 
-  const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+//   const app = initializeApp(firebaseConfig);
+// const messaging = getMessaging(app);
 
 onMessage(messaging, (payload) => {
   console.log('Foreground message received:', payload);
