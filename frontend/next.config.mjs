301f5e819c0ec995b722f3309b278dd134063db8
Tanvi Agarwal <agarwaltanvi1507@gmail.com>
2025-06-05 01:12:17 +0530

    fixes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,24 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// import withPWA from 'next-pwa';
-// const nextConfig = {};
-
-// export default withPWA({
-//   ...nextConfig,
-//   pwa: {
-//     dest: 'public',
-//     register: true,
-//     skipWaiting: true,
-//   },
-// });
 /** @type {import('next').NextConfig} */
 import withPWA from 'next-pwa';
-
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true, // since you're using /src/app
-  },
-};
+const nextConfig = {};
 
 export default withPWA({
   ...nextConfig,
@@ -26,7 +8,6 @@ export default withPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development', // optional: disables PWA in dev mode
   },
 });
 
