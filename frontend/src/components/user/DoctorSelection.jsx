f6cd2a810ebca670de9f25fd08009aa116811d8a
Kartikey Raghav <kartikeyraghav2006@gmail.com>
2025-06-05 15:15:29 +0530

    For email changes

--- frontend/src/components/user/DoctorSelection.jsx
+++ frontend/src/components/user/DoctorSelection.jsx
@@ -8,7 +8,7 @@ const DoctorSelectionStep = ({ doctors, onSelect }) => {
           {doctors.map((doctor) => (
             <div
               key={doctor.id}
-              className="group w-full bg-[var(--custom-white)] hover:bg-[var(--custom-orange-50)] p-6 rounded-2xl shadow-sm hover:shadow-md transition-all duration-300 transform hover:-translate-y-1 cursor-pointer border border-[var(--custom-orange-100)] hover:border-[var(--custom-orange-200)]"
+              className="group w-full bg-[var(--custom-white)] max-w-md hover:bg-[var(--custom-orange-50)] p-6 rounded-2xl shadow-sm hover:shadow-md transition-all duration-300 transform hover:-translate-y-1 cursor-pointer border border-[var(--custom-orange-100)] hover:border-[var(--custom-orange-200)]"
               onClick={() => onSelect(doctor)}
             >
               <div className="flex w-full flex-col justify-center items-center sm:flex-row gap-3 sm:gap-6">
@@ -22,7 +22,7 @@ const DoctorSelectionStep = ({ doctors, onSelect }) => {
                       />
                     ) : (
                       <div className="p-4">
-                        <User className="w-8 h-8  text-[var(--custom-orange-700)]" />
+                        <User className="w-8 h-8 text-[var(--custom-orange-700)]" />
                       </div>
                     )}
                   </div>
@@ -35,7 +35,7 @@ const DoctorSelectionStep = ({ doctors, onSelect }) => {
                   <p className="text-center sm:text-left text-sm text-[var(--custom-gray-600)] line-clamp-2">
                     {doctor.desc || "No description available."}
                   </p>
-                  <div className="flex-1 space-y-2 mt-5 sm:mt-3">
+                  <div className="flex-1 space-y-2 mt-5 sm:mt-3 text-left">
                     <div className="flex items-center gap-2 text-sm text-[var(--custom-gray-600)] group-hover:text-[var(--custom-orange-700)] transition-colors duration-300">
                       <div className="bg-[var(--custom-orange-100)] p-1.5 rounded-full">
                         <Phone className="w-4 h-4" />
@@ -47,6 +47,12 @@ const DoctorSelectionStep = ({ doctors, onSelect }) => {
                         <Mail className="w-4 h-4" />
                       </div>
                       <span>{doctor.email}</span>
+                    </div>
+                    <div className="flex items-center gap-2 text-sm text-[var(--custom-gray-600)] group-hover:text-[var(--custom-orange-700)] transition-colors duration-300">
+                      <div className="bg-[var(--custom-orange-100)] p-1.5 rounded-full">
+                        <MapPin className="w-4 h-4" />
+                      </div>
+                      <span>{doctor.office_address || "IIT Indore"}</span>
                     </div>
                   </div>
                 </div>
