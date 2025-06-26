bff575052b677f9278dfbf2838b6e0a7e37cd76e
NSuryansh <nagar.suryansh@gmail.com>
2025-06-26 19:48:29 +0530

    changes

--- modifiedrun.py
+++ modifiedrun.py
@@ -262,8 +262,8 @@ def analyze_user():
             # Load and filter CSV data
             data = pd.read_csv('tmp/memory.csv', dtype={'user_id': str})
             # print(data, "dara")
-            user_data = data[data['user_id'] == int(user_id)]
-            # print(user_data, "AEEE HALLLLLo")
+            user_data = data[data['user_id'] == str(user_id)]
+            print(user_data, "AEEE HALLLLLo")
             if user_data.empty:
                 print("empty data")
                 return jsonify({"error": "User not found"}), 404
